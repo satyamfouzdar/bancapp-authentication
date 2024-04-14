347aa8d17e7d9d847731406de51536a86e0424d7
Satyam Faujdar <satyam.faujdar@alcortech.com>
2024-04-15 00:22:10 +0530

    Add basic Styles and Forms

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent }, // Dashboard as the root path
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
+  { path: '**', redirectTo: '' }
+];
